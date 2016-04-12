342e5e7ff3ef5e167bcbf7e65b06b02a1ef153cd
Phunda <stevkiev@gmail.com>
2016-04-12 23:05:38 +0200

    home dropdown buttons
    sensoren en huisgenoten

--- home.php
+++ home.php
@@ -23,7 +23,7 @@
 
             <!--Navigatiebalk -->
             <nav class="uk-navbar uk-margin-large-bottom" data-uk-sticky="clsactive: 'uk-navbar-attached'">
-                <a class="uk-navbar-brand uk-hidden-small" href="layouts_frontpage.html">Mijn Dorm</a>
+                <a class="uk-navbar-brand uk-hidden-small" href="layouts_frontpage.html">StuDorm</a>
                 <ul class="uk-navbar-nav uk-hidden-small">
                     <li class="uk-active">
                         <a href="layouts_frontpage.html">Frontpage</a>
@@ -45,20 +45,49 @@
                     </li>
                 </ul>
             <a href="#offcanvas" class="uk-navbar-toggle uk-visible-small" data-uk-offcanvas></a>
-                <div class="uk-navbar-brand uk-navbar-center uk-visible-small">Mijn Dorm</div>
+                <div class="uk-navbar-brand uk-navbar-center uk-visible-small">StuDorm</div>
             </nav>
 
 
             
             <div class="uk-grid uk-margin-top">
                 <!--Buttons huisgenoten en sensoren -->
-                <div class="uk-width-1-2 uk-margin-bottom uk-container-center">
-                    <button class="uk-button uk-width-1-1" type="button">Huisgenoten</button>
-                </div>
+				
+                <div class="uk-width-7-10 uk-margin-bottom uk-container-center">
 
-                <div class="uk-width-1-2 uk-container-center">
-                    <button class="uk-button uk-width-1-1" type="button">Sensoren</button>
-                </div>
+				
+								<div class="uk-button-dropdown uk-margin-bottom" data-uk-dropdown="{mode:'click'}">
+                                    <button class="uk-button">Huisgenoten <i class="uk-icon-caret-down"></i></button>
+                                    <div class="uk-dropdown">
+                                        <ul class="uk-nav uk-nav-dropdown">
+                                            <li class="uk-nav-header">Aanwezige huisgenoten:</li>
+                                            <li><a href="#"><i class="uk-icon-check"></i> Marieke Bos</a></li>
+                                            <li><a href="#"><i class="uk-icon-check"></i> Tim Nijgaard</a></li>
+                                            <li class="uk-nav-divider"></li>
+											<li class="uk-nav-header">Niet aanwezig:</li>
+                                            <li><a href="#"><i class="uk-icon-times"></i> Koen Bakker</a></li>
+											<li><a href="#"><i class="uk-icon-times"></i> Nadja Regter</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+								
+								<div class="uk-button-dropdown uk-margin-bottom" data-uk-dropdown="{mode:'click'}">
+                                    <button class="uk-button">Sensoren <i class="uk-icon-caret-down"></i></button>
+                                    <div class="uk-dropdown">
+                                        <ul class="uk-nav uk-nav-dropdown">
+                                            <li class="uk-nav-header">Sensoren</li>
+                                            <li><a href="#"><i class="uk-icon-check"></i> Woonkamer</a></li>
+                                            <li><a href="#"><i class="uk-icon-check"></i> Keuken</a></li>
+											<li><a href="#"><i class="uk-icon-check"></i> Eigen slaapkamer</a></li>
+                                            <li class="uk-nav-divider"></li>
+											<li class="uk-nav-header">Defecte sensoren</li>
+											<li><a href="#"><i class="uk-icon-times"></i> Badkamer</a></li>
+                                        </ul>
+                                    </div>
+                                </div>	
+								
+				</div>
+
                 <!--Einde buttons huisgenoten en sensoren -->
 				
 				
