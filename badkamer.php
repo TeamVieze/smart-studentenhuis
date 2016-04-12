c87b63f0ca269b7df6bd804cb15111a1e84eb4d5
Phunda <stevkiev@gmail.com>
2016-04-12 14:06:52 +0200

    cover foto voor badkamer toegevoegd
    + reserveringen titel

--- badkamer.php
+++ badkamer.php
@@ -63,9 +63,10 @@
             </div>
 			
 			<div class="uk-grid" data-uk-grid-margin>
+			
                 <div class="uk-width-medium-1-1">
 
-                    <div class="uk-vertical-align uk-text-center" style="background: url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4NCjwhLS0gR2VuZXJhdG9yOiBBZG9iZSBJbGx1c3RyYXRvciAxNi4wLjQsIFNWRyBFeHBvcnQgUGx1Zy1JbiAuIFNWRyBWZXJzaW9uOiA2LjAwIEJ1aWxkIDApICAtLT4NCjwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+DQo8c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkViZW5lXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4Ig0KCSB3aWR0aD0iMTEzMHB4IiBoZWlnaHQ9IjQ1MHB4IiB2aWV3Qm94PSIwIDAgMTEzMCA0NTAiIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDExMzAgNDUwIiB4bWw6c3BhY2U9InByZXNlcnZlIj4NCjxyZWN0IGZpbGw9IiNGNUY1RjUiIHdpZHRoPSIxMTMwIiBoZWlnaHQ9IjQ1MCIvPg0KPC9zdmc+DQo=') 50% 0 no-repeat; height: 450px;">
+                    <div class="uk-vertical-align uk-text-center" style="background: url('images/bathroom.jpg') no-repeat; background-size: cover; height: 500px;">
                         <div class="uk-vertical-align-middle uk-width-1-2">
                             <h1 class="uk-heading-large">Op dit moment: <div class="uk-alert uk-alert-danger">BEZET</div></h1>
                             <p class="uk-text-large">Vrij over c.a.:</p>
@@ -77,11 +78,9 @@
 
                 </div>
             </div>
+			<h2>Recente reserveringen: </h2>
 			
 			<div class="uk-grid">
-				
-				
-				
 				<div class="uk-width-large-1-3 uk-width-medium-1-2 uk-width-small-1-1 uk-margin-bottom">
                     <div class="uk-grid">
                         <div class="uk-width-1-6">
